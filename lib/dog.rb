2f6fe3893a7a99b2df135c7eb8cd82e4e5e0a338
Luke Worley <luke.worley@colorado.edu>
2018-05-22 16:30:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(breed = "Mutt")
+  def initialize(name, breed = "Mutt")
     @breed = breed
+    @name = name
   end
   
   def breed= (input)
@@ -9,5 +10,8 @@ class Dog
   
   def name
     @name
+  end
+  def breed
+    @breed
   end
 end
